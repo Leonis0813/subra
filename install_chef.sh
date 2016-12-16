50e3aea490c7d91a7a86d707ec5bb5842d00e5fc
root <root@localhost.localdomain>
2016-12-16 21:00:43 +0900

    create script for installing chef

---
+++ install_chef.sh
@@ -0,0 +1,4 @@
+yum -y install rsync openssh-clients
+wget https://opscode-omnibus-packages.s3.amazonaws.com/el/7/x86_64/chefdk-0.7.0-1.el7.x86_64.rpm
+rpm -ivh chefdk-0.7.0-1.el7.x86_64.rpm
+rm -f chefdk-0.7.0-1.el7.x86_64.rpm
