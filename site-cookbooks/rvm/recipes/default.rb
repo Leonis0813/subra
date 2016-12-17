86c9ea9168823b9fb65bb5dbe11c78eb220b9474
Leonis0813 <Leonis.0813@gmail.com>
2016-12-17 10:06:10 +0900

    create recipe

--- site-cookbooks/rvm/recipes/default.rb
+++ site-cookbooks/rvm/recipes/default.rb
@@ -6,3 +6,10 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+execute 'install rvm' do
+  command <<-"EOF"
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | sudo bash -s stable
+rvm reload
+  EOF
+end
