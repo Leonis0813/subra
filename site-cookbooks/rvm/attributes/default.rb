283d860611daf7ad78979e6064c0253c755040d8
Leonis0813 <Leonis.0813@gmail.com>
2016-12-17 10:12:02 +0900

    create attribute

---
+++ site-cookbooks/rvm/attributes/default.rb
@@ -0,0 +1,5 @@
+default[:gpg] = {
+  :server => 'hkp://keys.gnupg.net',
+  :key => '409B6B1796C275462A1703113804BB82D39DC0E3',
+}
+default[:rvm][:server] = 'https://get.rvm.io'
--- site-cookbooks/rvm/recipes/default.rb
+++ site-cookbooks/rvm/recipes/default.rb
@@ -8,8 +8,8 @@
 #
 execute 'install rvm' do
   command <<-"EOF"
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | sudo bash -s stable
+gpg --keyserver #{node[:gpg][:server]} --recv-keys #{node[:gpg][:key]}
+curl -sSL #{node[:rvm][:server] | sudo bash -s stable
 rvm reload
   EOF
 end
