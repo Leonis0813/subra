68d789a74b023705abf0d99eae5deac6cd5b897a
Leonis0813 <Leonis.0813@gmail.com>
2016-12-18 14:48:36 +0900

    create recipe & add id_rsa.pub for developer

---
+++ site-cookbooks/user/recipes/create.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook Name:: user
+# Recipe:: create
+#
+# Copyright 2016, Leonis0813
+#
+# All rights reserved - Do Not Redistribute
+#
+user 'developer' do
+  home '/home/developer'
+  manage_home true
+  action :create
+end
+
+directory '/home/developer/.ssh' do
+  owner 'developer'
+  group 'developer'
+  mode 0700
+  action :create
+end
+
+file '/home/developer/.ssh/authorized_keys' do
+  content IO.read(File.absolute_path(File.dirname(__FILE__) + '/../files/default/developer/id_rsa.pub'))
+  owner 'developer'
+  group 'developer'
+  mode '0644'
+  action :create
+end
