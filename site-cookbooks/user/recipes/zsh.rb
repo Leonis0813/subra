fab17f326f24acbdc5f8ca04c559bba0e67a50f1
Leonis <Leonis.0813@gmail.com>
2016-12-22 23:17:37 +0900

    create recipe user::zsh (#22)
    * add shell option to user::create
    
    * create recipe

---
+++ site-cookbooks/user/recipes/zsh.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: user
+# Recipe:: zsh
+#
+# Copyright 2016, Leonis0813
+#
+# All rights reserved - Do Not Redistribute
+#
+file "#{node[:user][:home]}/.zshrc" do
+  content IO.read(File.absolute_path(File.dirname(__FILE__) + '/../../zsh/files/default/zshrc'))
+  owner node[:user][:name]
+  group node[:user][:name]
+  mode 0644
+  action :create
+end
