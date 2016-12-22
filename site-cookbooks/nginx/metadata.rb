9105245aa22d256fbfe0f3d2679c98c88903578a
Leonis0813 <Leonis.0813@gmail.com>
2016-12-22 23:51:49 +0900

    update docs

--- site-cookbooks/nginx/metadata.rb
+++ site-cookbooks/nginx/metadata.rb
@@ -1,6 +1,5 @@
 name             'nginx'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Leonis0813'
 license          'All rights reserved'
 description      'Installs/Configures nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
