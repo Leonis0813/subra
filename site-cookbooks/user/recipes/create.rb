3a3a45e89300166d270b5db60203340f958b9385
Leonis0813 <Leonis.0813@gmail.com>
2016-12-22 23:19:51 +0900

    remove ssh settings from create

--- site-cookbooks/user/recipes/create.rb
+++ site-cookbooks/user/recipes/create.rb
@@ -13,18 +13,3 @@ user node[:user][:name] do
   shell '/bin/zsh'
   action :create
 end
-
-directory "#{node[:user][:home]}/.ssh" do
-  owner node[:user][:name]
-  group node[:user][:name]
-  mode 0700
-  action :create
-end
-
-file "#{node[:user][:home]}/.ssh/authorized_keys" do
-  content IO.read(File.absolute_path(File.dirname(__FILE__) + "/../files/default/#{node[:user][:name]}/id_rsa.pub"))
-  owner node[:user][:name]
-  group node[:user][:name]
-  mode 0644
-  action :create
-end
