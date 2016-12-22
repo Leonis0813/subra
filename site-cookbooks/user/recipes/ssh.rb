6564b1f7384fd9ca5c3d010a9b4b009d6d33a56f
Leonis0813 <Leonis.0813@gmail.com>
2016-12-22 23:19:07 +0900

    create recipe

---
+++ site-cookbooks/user/recipes/ssh.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: user
+# Recipe:: ssh
+#
+# Copyright 2016, Leonis0813
+#
+# All rights reserved - Do Not Redistribute
+#
+directory "#{node[:user][:home]}/.ssh" do
+  owner node[:user][:name]
+  group node[:user][:name]
+  mode 0700
+  action :create
+end
+
+file "#{node[:user][:home]}/.ssh/authorized_keys" do
+  content IO.read(File.absolute_path(File.dirname(__FILE__) + "/../files/default/#{node[:user][:name]}/id_rsa.pub"))
+  owner node[:user][:name]
+  group node[:user][:name]
+  mode 0644
+  action :create
+end
