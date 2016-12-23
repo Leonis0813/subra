3be131df04488addcc78fd4077354fbcb291a820
Leonis0813 <Leonis.0813@gmail.com>
2016-12-23 17:38:16 +0900

    iterate

--- site-cookbooks/rvm/recipes/install.rb
+++ site-cookbooks/rvm/recipes/install.rb
@@ -7,7 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 unless File.exists?(node[:rvm][:install_dir])
-  execute "curl -sSL #{node[:gpg][:server]} | gpg --import -"
-  execute "curl -sSL #{node[:rvm][:server]} | sudo bash -s stable"
-  execute "#{node[:rvm][:install_dir]}/bin/rvm reload"
+  [
+    "curl -sSL #{node[:gpg][:server]} | gpg --import -",
+    "curl -sSL #{node[:rvm][:server]} | sudo bash -s stable",
+    "#{node[:rvm][:install_dir]}/bin/rvm reload",
+  ].each do |command|
+    execute command
+  end
 end
