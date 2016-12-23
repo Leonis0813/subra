4958bd74446215f833704f6c4b2c9a09b9bf44ef
Leonis0813 <Leonis.0813@gmail.com>
2016-12-23 22:33:33 +0900

    use git_installed

--- site-cookbooks/git/recipes/install.rb
+++ site-cookbooks/git/recipes/install.rb
@@ -6,23 +6,25 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-package node[:git][:requirements] do
-  action :install
-end
+unless git_installed?
+  package node[:git][:requirements] do
+    action :install
+  end
 
-remote_file "/tmp/git-#{node[:git][:version]}.tar.gz" do
-  source "https://www.kernel.org/pub/software/scm/git/git-#{node[:git][:version]}.tar.gz"
-  not_if { File.exists?("/tmp/git-#{node[:git][:version]}.tar.gz") }
-end
+  remote_file "/tmp/git-#{node[:git][:version]}.tar.gz" do
+    source "https://www.kernel.org/pub/software/scm/git/git-#{node[:git][:version]}.tar.gz"
+    not_if { File.exists?("/tmp/git-#{node[:git][:version]}.tar.gz") }
+  end
 
-execute "tar zxf git-#{node[:git][:version]}.tar.gz" do
-  cwd '/tmp'
-  not_if { File.exists?("/tmp/git-#{node[:git][:version]}") }
-end
+  execute "tar zxf git-#{node[:git][:version]}.tar.gz" do
+    cwd '/tmp'
+    not_if { File.exists?("/tmp/git-#{node[:git][:version]}") }
+  end
 
-['make prefix=/usr/local all', 'make prefix=/usr/local install'].each do |command|
-  execute command do
-    cwd "/tmp/git-#{node[:git][:version]}"
-    only_if { File.exists?("/tmp/git-#{node[:git][:version]}") }
+  ['make prefix=/usr/local all', 'make prefix=/usr/local install'].each do |command|
+    execute command do
+      cwd "/tmp/git-#{node[:git][:version]}"
+      only_if { File.exists?("/tmp/git-#{node[:git][:version]}") }
+    end
   end
 end
