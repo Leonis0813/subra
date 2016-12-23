83f589d475e77d58a2007384dd46107a69cb0f2f
Leonis0813 <Leonis.0813@gmail.com>
2016-12-23 19:08:36 +0900

    fix bug

--- site-cookbooks/nginx/recipes/install.rb
+++ site-cookbooks/nginx/recipes/install.rb
@@ -12,18 +12,18 @@ end
 
 remote_file "/tmp/nginx-#{node[:nginx][:version]}.tar.gz" do
   source "https://nginx.org/download/nginx-#{node[:nginx][:version]}.tar.gz"
-  not_if { File.exists("/tmp/nginx-#{node[:nginx][:version]}.tar.gz") }
+  not_if { File.exists?("/tmp/nginx-#{node[:nginx][:version]}.tar.gz") }
 end
 
 execute "tar zxf nginx-#{node[:nginx][:version]}.tar.gz" do
   cwd '/tmp'
-  not_if { File.exists("/tmp/nginx-#{node[:nginx][:version]}") }
+  not_if { File.exists?("/tmp/nginx-#{node[:nginx][:version]}") }
 end
 
 ["./configure --prefix=#{node[:nginx][:install_dir]}", 'make', 'sudo make install'].each do |command|
   execute command do
     cwd "/tmp/nginx-#{node[:nginx][:version]}"
-    only_if { File.exists("/tmp/nginx-#{node[:nginx][:version]}") }
+    only_if { File.exists?("/tmp/nginx-#{node[:nginx][:version]}") }
   end
 end
 
