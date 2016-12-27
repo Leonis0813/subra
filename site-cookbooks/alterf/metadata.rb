0e2502e636add45e3390622ada42c94578f5e07e
Leonis0813 <Leonis.0813@gmail.com>
2016-12-27 13:30:37 +0100

    create cookbook

---
+++ site-cookbooks/alterf/metadata.rb
@@ -0,0 +1,7 @@
+name             'alterf'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures alterf'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
