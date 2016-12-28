ea37d78bce82951152be1b8e7d34ef8eeb7e471e
Leonis0813 <Leonis.0813@gmail.com>
2016-12-28 15:30:07 +0100

    create cron recipe

--- site-cookbooks/alterf/attributes/default.rb
+++ site-cookbooks/alterf/attributes/default.rb
@@ -1,3 +1,13 @@
 default[:alterf][:app_name] = 'alterf'
 default[:alterf][:deploy_dir] = "/opt/#{default[:alterf][:app_name]}"
 default[:alterf][:repository] = "https://github.com/Leonis0813/#{default[:alterf][:app_name]}.git"
+default[:alterf][:cron_settings] = [
+  {:name => 'import', :hour => '0', :minute => '0'},
+  {:name => 'aggregation', :hour => '0', :minute => '5'},
+]
+default[:alterf][:cron_paths] = [
+  '/usr/local/rvm/gems/ruby-2.2.0@global/bin',
+  '/usr/local/rvm/rubies/ruby-2.2.0/bin',
+  '/usr/bin',
+  '/bin',
+]
