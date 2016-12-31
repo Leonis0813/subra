4327ee8c862b6e9981800f7af358e03f9cfdcef6
Leonis0813 <Leonis.0813@gmail.com>
2016-12-31 11:56:11 +0100

    update deploy recipe

--- site-cookbooks/alterf/recipes/deploy.rb
+++ site-cookbooks/alterf/recipes/deploy.rb
@@ -14,9 +14,28 @@ deploy node[:alterf][:deploy_dir] do
   create_dirs_before_symlink.clear
   purge_before_symlink.clear
   symlink_before_migrate.clear
-  symlinks 'log' => 'log'
+  symlinks 'log' => 'log', 'back_up' => 'back_up'
 
   before_migrate do
-    directory File.join(release_path, '../../shared/log')
+    directory File.join(release_path, 'vendor')
+
+    %w[ back_up/race_list back_up/races back_up/horses log bundle ].each do |dir|
+      directory File.join(release_path, "../../shared/#{dir}") do
+        recursive true
+      end
+    end
+
+    package 'mysql-devel' do
+      action :install
+    end
+
+    link File.join(release_path, 'vendor/bundle') do
+      to File.join(release_path, '../../shared/bundle')
+      link_type :symbolic
+    end
+
+    execute 'bundle install --path=vendor/bundle' do
+      cwd release_path
+    end
   end
 end
