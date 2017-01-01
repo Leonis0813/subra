5c6884e0a145978ba87f204458190d2e50a0a6e4
Leonis0813 <Leonis.0813@gmail.com>
2017-01-01 13:55:47 +0100

    update recipe

---
+++ site-cookbooks/regulus/attributes/default.rb
@@ -0,0 +1,4 @@
+default[:regulus][:app_name] = 'regulus'
+default[:regulus][:deploy_dir] = "/opt/#{default[:regulus][:app_name]}"
+default[:regulus][:repository] = "https://github.com/Leonis0813/#{default[:regulus][:app_name]}.git"
+default[:regulus][:branch] = 'develop'
--- site-cookbooks/regulus/recipes/deploy.rb
+++ site-cookbooks/regulus/recipes/deploy.rb
@@ -6,3 +6,13 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+deploy node[:regulus][:deploy_dir] do
+  repo node[:regulus][:repository]
+  branch node[:regulus][:branch]
+  action :deploy
+  migrate false
+  symlink_before_migrate.clear
+  create_dirs_before_symlink.clear
+  purge_before_symlink.clear
+  symlinks.clear
+end
