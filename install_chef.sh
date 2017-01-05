b952080d838edd4ac16fa43edafb0890d5eddc7f
Leonis0813 <Leonis.0813@gmail.com>
2017-01-05 22:39:33 +0900

    select version

--- install_chef.sh
+++ install_chef.sh
@@ -1,4 +1,5 @@
+VERSION=$1
 yum -y install rsync openssh-clients
-wget https://opscode-omnibus-packages.s3.amazonaws.com/el/7/x86_64/chefdk-0.7.0-1.el7.x86_64.rpm
-rpm -ivh chefdk-0.7.0-1.el7.x86_64.rpm
-rm -f chefdk-0.7.0-1.el7.x86_64.rpm
+wget https://opscode-omnibus-packages.s3.amazonaws.com/el/${VERSION}/x86_64/chefdk-0.7.0-1.el${VERSION}.x86_64.rpm
+rpm -ivh chefdk-0.7.0-1.el${VERSION}.x86_64.rpm
+rm -f chefdk-0.7.0-1.el${VERSION}.x86_64.rpm
