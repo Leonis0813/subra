c1a6b3f0a66a06983f06726cd08d4cbd586f787d
Leonis0813 <Leonis.0813@gmail.com>
2017-01-05 20:41:34 +0900

    create recipe cron

---
+++ site-cookbooks/regulus/recipes/cron.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: regulus
+# Recipe:: cron
+#
+# Copyright 2017, Leonis0813
+#
+# All rights reserved - Do Not Redistribute
+#
+node[:regulus][:cron_settings].each do |cron_setting|
+  cron "#{node[:regulus][:app_name]} #{cron_setting[:name]}" do
+    command [
+      'cd /opt/regulus/current &&',
+      "bundle exec ruby #{cron_setting[:name]}.rb",
+    ].join(' ')
+    hour cron_setting[:hour]
+    minute cron_setting[:minute]
+    path node[:alterf][:cron_paths].join(':')
+  end
+end
