8e11c711608f30ebed50b409aad2cd0470eccdf1
Leonis <Leonis.0813@gmail.com>
2017-01-07 11:20:43 +0900

    select version (#35)
    * select version
    
    * install if arg
    
    * install if 6 or 7

--- install_chef.sh
+++ install_chef.sh
@@ -1,4 +1,9 @@
-yum -y install rsync openssh-clients
-wget https://opscode-omnibus-packages.s3.amazonaws.com/el/7/x86_64/chefdk-0.7.0-1.el7.x86_64.rpm
-rpm -ivh chefdk-0.7.0-1.el7.x86_64.rpm
-rm -f chefdk-0.7.0-1.el7.x86_64.rpm
+if [ $# -eq 1 ];then
+  if [ $1 -eq 6 -o $1 -eq 7 ];then
+    VERSION=$1
+    yum -y install rsync openssh-clients
+    wget https://opscode-omnibus-packages.s3.amazonaws.com/el/${VERSION}/x86_64/chefdk-0.7.0-1.el${VERSION}.x86_64.rpm
+    rpm -ivh chefdk-0.7.0-1.el${VERSION}.x86_64.rpm
+    rm -f chefdk-0.7.0-1.el${VERSION}.x86_64.rpm
+  fi
+fi
