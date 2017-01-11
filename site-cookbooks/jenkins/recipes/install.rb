cee4ba05113e8ffb60e0c38b9d498e8630325c11
Leonis0813 <Leonis.0813@gmail.com>
2017-01-11 22:30:41 +0900

    add yum_package resource

--- site-cookbooks/jenkins/recipes/install.rb
+++ site-cookbooks/jenkins/recipes/install.rb
@@ -1,8 +1,13 @@
 #
 # Cookbook Name:: jenkins
-# Recipe:: default
+# Recipe:: install
 #
-# Copyright 2017, YOUR_COMPANY_NAME
+# Copyright 2017, Leonis0813
 #
 # All rights reserved - Do Not Redistribute
 #
+yum_repository 'jenkins' do
+  description 'Jenkins stable repository'
+  baseurl 'http://pkg.jenkins-ci.org/redhat/jenkins.repo'
+  gpgkey 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key'
+end
