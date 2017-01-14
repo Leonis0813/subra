2d3d8beba1e68345822d2d2981d248b9df84df1c
Leonis0813 <Leonis.0813@gmail.com>
2017-01-14 23:53:14 +0000

    add to sudoers

--- site-cookbooks/jenkins/recipes/install.rb
+++ site-cookbooks/jenkins/recipes/install.rb
@@ -19,6 +19,14 @@ package 'jenkins' do
   not_if 'rpm -q jenkins'
 end
 
+file '/etc/sudoers.d/jenkins' do
+  content IO.read(File.absolute_path(File.dirname(__FILE__) + '/../files/default/jenkins'))
+  owner 'root'
+  group 'root'
+  mode 0440
+  action :create
+end
+
 service 'jenkins' do
   action [:enable, :start]
 end
