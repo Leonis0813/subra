cf0fa5ebc49da507df12fb583c53f3f8e632345e
Leonis <Leonis.0813@gmail.com>
2017-01-21 17:16:38 +0900

    Update nodes (#46)
    * create role alterf
    
    * create role regulus
    
    * use role
    
    * modify jenkins config
    
    * add jenkins recipes to node
    
    * install bundler in global

--- site-cookbooks/regulus/recipes/ruby.rb
+++ site-cookbooks/regulus/recipes/ruby.rb
@@ -6,7 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-['rvm install 2.2.0', 'rvm 2.2.0 do gem install bundler'].each do |command|
+['rvm install 2.2.0', 'rvm 2.2.0@global do gem install bundler'].each do |command|
   execute command do
     environment 'PATH' => '/usr/local/rvm/bin:/usr/bin:/bin'
   end
