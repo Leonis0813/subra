ea76a86ef625a972f052b0b42fe598c85894df8e
Leonis0813 <Leonis.0813@gmail.com>
2017-01-22 05:09:06 +0000

    aggregation to aggregate

--- site-cookbooks/alterf/attributes/default.rb
+++ site-cookbooks/alterf/attributes/default.rb
@@ -6,7 +6,7 @@ default[:alterf][:symlinks] = {'log' => 'log', 'backup' => 'backup'}
 default[:alterf][:shared_dirs] = %w[ backup/race_list backup/races backup/horses log bundle ]
 default[:alterf][:cron_settings] = [
   {:name => 'import', :hour => '0', :minute => '0'},
-  {:name => 'aggregation', :hour => '0', :minute => '5'},
+  {:name => 'aggregate', :hour => '0', :minute => '5'},
 ]
 default[:alterf][:cron_paths] = [
   '/usr/local/rvm/gems/ruby-2.2.0@global/bin',
