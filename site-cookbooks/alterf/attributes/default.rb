c989527f88973be7c158d671471e90e055f6dd14
Leonis0813 <Leonis.0813@gmail.com>
2017-01-22 05:04:25 +0000

    rename cron name

--- site-cookbooks/alterf/attributes/default.rb
+++ site-cookbooks/alterf/attributes/default.rb
@@ -1,12 +1,12 @@
 default[:alterf][:app_name] = 'alterf'
 default[:alterf][:deploy_dir] = "/opt/#{default[:alterf][:app_name]}"
 default[:alterf][:repository] = "https://github.com/Leonis0813/#{default[:alterf][:app_name]}.git"
-default[:alterf][:branch] = 'develop'
+default[:alterf][:branch] = 'fix-aggregation'
 default[:alterf][:symlinks] = {'log' => 'log', 'backup' => 'backup'}
 default[:alterf][:shared_dirs] = %w[ backup/race_list backup/races backup/horses log bundle ]
 default[:alterf][:cron_settings] = [
   {:name => 'import', :hour => '0', :minute => '0'},
-  {:name => 'aggregate', :hour => '0', :minute => '5'},
+  {:name => 'aggregation', :hour => '0', :minute => '5'},
 ]
 default[:alterf][:cron_paths] = [
   '/usr/local/rvm/gems/ruby-2.2.0@global/bin',
