928fd99fec9cea60294a7373ac9725aa1966fb02
Leonis <Leonis.0813@gmail.com>
2017-01-23 20:57:23 +0900

    Build docs (#49)
    * create cookbook
    
    * update docs
    
    * rename
    
    * remove cookbook chort
    
    * install requirements
    
    * install by pip
    
    * add jenkins config for chort
    
    * add location for docs
    
    * add copy command to config
    
    * add line

--- site-cookbooks/sphinx/recipes/install.rb
+++ site-cookbooks/sphinx/recipes/install.rb
@@ -6,8 +6,10 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-package 'python-pip' do
+package %w[ python-pip graphviz ] do
   action :install
 end
 
-execute 'pip install sphinx'
+%w[ sphinx==1.4.5 sphinxcontrib-plantuml sphinxcontrib-httpdomain ].each do |package|
+  execute "pip install #{package}"
+end
