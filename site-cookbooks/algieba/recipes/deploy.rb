a385400ba0a0dbae2a70cf3d14b49604b62bd25b
Leonis <Leonis.0813@gmail.com>
2017-01-29 22:52:45 +0900

    Add jenkins jobs for test (#54)
    * add coloring settings
    
    * install Xvfb
    
    * add dbus-uuidgen command
    
    * modify rpm command
    
    * add test job configs
    
    * add jobs to attribute

--- site-cookbooks/algieba/recipes/deploy.rb
+++ site-cookbooks/algieba/recipes/deploy.rb
@@ -72,3 +72,17 @@ deploy node[:algieba][:deploy_dir] do
     restart_command 'rvm 2.2.0 do bundle exec rake unicorn:start'
   end
 end
+
+if node[:algieba][:environment] == 'development'
+  execute 'yum -y groupupdate "X Window System"' do
+    not_if 'rpm -q xorg-x11-server-Xvfb'
+  end
+
+  package 'xorg-x11-server-Xvfb' do
+    not_if 'rpm -q xorg-x11-server-Xvfb'
+  end
+
+  execute 'dbus-uuidgen > /var/lib/dbus/machine-id' do
+    not_if 'rpm -q xorg-x11-server-Xvfb'
+  end
+end
