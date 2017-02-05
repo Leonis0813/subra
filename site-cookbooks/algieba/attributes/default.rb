4c6e825b5980da8421b9e99d1d84a0ce4c51d802
Leonis <developer@tk2-209-14108.vs.sakura.ne.jp>
2017-02-05 22:19:34 +0900

    create bundle directory

--- site-cookbooks/algieba/attributes/default.rb
+++ site-cookbooks/algieba/attributes/default.rb
@@ -3,5 +3,5 @@ default[:algieba][:deploy_dir] = "/opt/nginx/apps/#{default[:algieba][:app_name]
 default[:algieba][:repository] = "https://github.com/Leonis0813/#{default[:algieba][:app_name]}.git"
 default[:algieba][:branch] = 'develop'
 default[:algieba][:symlinks] = {'log' => 'log', 'tmp' => 'tmp'}
-default[:algieba][:shared_dirs] = %w[ log tmp ]
+default[:algieba][:shared_dirs] = %w[ log tmp bundle ]
 default[:algieba][:ruby_version] = '2.2.0'
