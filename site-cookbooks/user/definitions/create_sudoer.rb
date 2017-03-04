bc865d5d9184276249a4746faaa921be9a1f0733
Leonis0813 <Leonis.0813@gmail.com>
2017-03-04 11:15:55 +0000

    modify definition

--- site-cookbooks/user/definitions/create_sudoer.rb
+++ site-cookbooks/user/definitions/create_sudoer.rb
@@ -1,9 +1,10 @@
 define :create_sudoer do
-  template "/etc/sudoers.d/#{user_name}" do
+  template "/etc/sudoers.d/#{params[:name]}" do
+    cookbook 'user'
     source 'sudoers.erb'
-    owner node[:regulus][:username]
-    group node[:regulus][:username]
-    mode 0600
+    owner 'root'
+    group 'root'
+    mode 0440
     variables(:user_name => params[:name])
   end
 end
