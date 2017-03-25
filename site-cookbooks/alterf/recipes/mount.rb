60398057c89b0871ab8d1e1d814ab3e2b8463a8b
Leonis0813 <Leonis.0813@gmail.com>
2017-03-25 10:13:27 +0000

    create mount directory

--- site-cookbooks/alterf/recipes/mount.rb
+++ site-cookbooks/alterf/recipes/mount.rb
@@ -6,14 +6,14 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-directory node[:alterf][:mount_path] do
-  user 'root'
-  group 'root'
-  mode 0755
-  not_if { File.exists?(node[:alterf][:mount_path]) }
-end
-
 node[:alterf][:mount_settings].each do |setting|
+  directory setting[:path] do
+    user 'root'
+    group 'root'
+    mode 0755
+    not_if { File.exists?(setting[:path]) }
+  end
+
   mount setting[:path] do
     fstype setting[:fstype]
     device setting[:device]
