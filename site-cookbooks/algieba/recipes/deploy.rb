8b26bb6332cfa45e24397bd456a1387231e4aa29
Leonis0813 <Leonis.0813@gmail.com>
2017-08-08 12:18:15 +0100

    fix unicorn stop bug

--- site-cookbooks/algieba/recipes/deploy.rb
+++ site-cookbooks/algieba/recipes/deploy.rb
@@ -72,7 +72,7 @@ deploy node[:algieba][:deploy_dir] do
     execute 'rvm 2.2.0 do bundle exec rake unicorn:stop' do
       cwd release_path
       environment 'RAILS_ENV' => node.chef_environment, 'PATH' => node[:rvm][:path]
-      only_if { File.exists?("#{node[:algieba][:deploy_dir]}/shared/tmp/pids/unicorn.pid") }
+      only_if "pgrep -lf 'unicorn_rails.*#{node[:algieba][:app_name]}*'"
     end
 
     execute 'rvm 2.2.0 do bundle exec rake unicorn:start' do
