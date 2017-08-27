02961a5e9cc3163884ffb2218c9f4108165caa3f
Leonis <Leonis.0813@gmail.com>
2017-08-27 19:26:46 +0900

    Regulus create settings yml (#91)
    * add template resource
    
    * create template
    
    * create results dir
    
    * add data bag to regulus
    
    * update CHANGELOG.md

--- site-cookbooks/regulus/attributes/default.rb
+++ site-cookbooks/regulus/attributes/default.rb
@@ -2,8 +2,8 @@ default[:regulus][:app_name] = 'regulus'
 default[:regulus][:deploy_dir] = "/opt/#{default[:regulus][:app_name]}"
 default[:regulus][:repository] = "https://github.com/Leonis0813/#{default[:regulus][:app_name]}.git"
 default[:regulus][:branch] = 'develop'
-default[:regulus][:symlinks] = {'tmp' => 'tmp', 'log' => 'log', 'backup' => 'backup'}
-default[:regulus][:shared_dirs] = %w[ tmp backup log bundle ]
+default[:regulus][:symlinks] = {'tmp' => 'tmp', 'log' => 'log', 'backup' => 'backup', 'results' => 'results'}
+default[:regulus][:shared_dirs] = %w[ tmp backup log bundle results ]
 default[:regulus][:requirements] = %w[ mysql-devel ]
 default[:regulus][:username] = 'analyzer'
 default[:regulus][:smb] = {
