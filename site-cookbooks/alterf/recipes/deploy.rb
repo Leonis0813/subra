12476d5067f3b7d10eba76bceb25ed5b243245b6
Leonis0813 <Leonis.0813@gmail.com>
2017-12-31 02:56:41 +0000

    update deploy recipe for alterf

--- site-cookbooks/alterf/recipes/deploy.rb
+++ site-cookbooks/alterf/recipes/deploy.rb
@@ -41,12 +41,54 @@ deploy node[:alterf][:deploy_dir] do
       environment 'PATH' => node[:rvm][:path]
       not_if { node.chef_environment == 'compute' }
     end
+  end
 
+  before_restart do
     template File.join(release_path, 'analyze/settings.yml') do
       source 'settings.yml.erb'
       owner 'root'
       group 'root'
       mode 0644
+      only_if { node.chef_environment == 'compute' }
+    end
+
+    execute 'rvm 2.2.0 do bundle exec rake resque:worker:restart' do
+      cwd release_path
+      environment 'PATH' => node[:rvm][:path]
+      only_if { node.chef_environment == 'compute' }
+    end
+
+    %w[ user_name password ].each do |user_info|
+      gmail = Chef::EncryptedDataBagItem.load('alterf', 'gmail')
+      execute "sed -i -e s/GMAIL_#{user_info.upcase}/#{gmail[user_info]}/g config/initializers/action_mailer.rb" do
+        cwd release_path
+        only_if { node.chef_environment == 'compute' }
+      end
+    end
+
+    execute 'rvm 2.2.0 do bundle exec rake assets:precompile' do
+      cwd release_path
+      environment 'RAILS_ENV' => 'production', 'PATH' => node[:rvm][:path], 'RAILS_RELATIVE_URL_ROOT' => "/#{node[:algieba][:app_name]}"
+      only_if { node.chef_environment == 'compute' }
+    end
+
+    execute 'sed -i -e "s/<%= ENV\[\"SECRET_KEY_BASE\"\] %>/`rvm 2.2.0 do bundle exec rake secret`/g" config/secrets.yml' do
+      cwd release_path
+      environment 'RAILS_ENV' => 'production', 'PATH' => node[:rvm][:path]
+      only_if { node.chef_environment == 'compute' }
+    end
+  end
+
+  restart_command do
+    execute 'rvm 2.2.0 do bundle exec rake unicorn:stop' do
+      cwd release_path
+      environment 'RAILS_ENV' => 'production', 'PATH' => node[:rvm][:path]
+      only_if "pgrep -lf 'unicorn_rails.*#{node[:algieba][:app_name]}*'"
+    end
+
+    execute 'rvm 2.2.0 do bundle exec rake unicorn:start' do
+      cwd release_path
+      environment 'RAILS_ENV' => 'production', 'PATH' => node[:rvm][:path]
       only_if { node.chef_environment == 'compute' }
     end
   end
