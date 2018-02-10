b15cba4e7e25cf0befe508d0cb30f3dfdd6b93fc
Leonis0813 <Leonis.0813@gmail.com>
2018-02-10 06:26:37 +0000

    move to default

--- site-cookbooks/jenkins/recipes/default.rb
+++ site-cookbooks/jenkins/recipes/default.rb
@@ -42,5 +42,35 @@ execute 'netstat -ant | grep 8080 | grep LISTEN' do
 end
 
 service 'network' do
-  action [:restart]
+  action :restart
+end
+
+remote_file node[:jenkins][:cli_path] do
+  source node[:jenkins][:cli_url]
+  owner 'root'
+  group 'root'
+  mode 0755
+  not_if { File.exists?(node[:jenkins][:cli_path]) }
+end
+
+node[:jenkins][:accounts].each do |account|
+  execute "create account - #{account[:id]}" do
+    command <<-"EOF"
+echo 'jenkins.model.Jenkins.instance.securityRealm.createAccount("#{account[:id]}", "#{account[:password]}")' |
+java -jar #{node[:jenkins][:cli_path]} -s #{node[:jenkins][:host]} groovy = --username=#{node[:jenkins][:admin][:username]} --password-file=#{node[:jenkins][:admin][:password_file]}
+    EOF
+    user 'root'
+    retries 5
+    retry_delay 10
+  end
+end
+
+node[:jenkins][:plugins].each do |plugin|
+  execute "install plugin - #{plugin}" do
+    command <<-"EOF"
+java -jar #{node[:jenkins][:cli_path]} -s #{node[:jenkins][:host]} install-plugin #{plugin} --username=#{node[:jenkins][:admin][:username]} --password-file=#{node[:jenkins][:admin][:password_file]}
+    EOF
+    user 'root'
+    retries 5
+  end
 end
