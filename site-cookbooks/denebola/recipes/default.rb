64a5598a73041c17d95883f07cd4183b6a1aa70e
Leonis0813 <Leonis.0813@gmail.com>
2018-03-11 12:44:54 +0000

    add migrate command

--- site-cookbooks/denebola/recipes/default.rb
+++ site-cookbooks/denebola/recipes/default.rb
@@ -1,22 +1,21 @@
 #
 # Cookbook Name:: denebola
-# Recipe:: deploy
+# Recipe:: default
 #
 # Copyright 2016, Leonis0813
 #
 # All rights reserved - Do Not Redistribute
 #
-node[:denebola][:requirements].each do |package_name|
-  package package_name
-end
-
 deploy node[:denebola][:deploy_dir] do
   repo node[:denebola][:repository]
   branch ENV['DENEBOLA_VERSION'] || node[:denebola][:branch]
+  migrate true
   create_dirs_before_symlink.clear
   purge_before_symlink.clear
   symlink_before_migrate.clear
   symlinks node[:denebola][:symlinks]
+  migration_command "rvm #{node[:denebola][:ruby_version]} do bundle exec rake db:migrate"
+  environment 'RAILS_ENV' => node.chef_environment.sub('compute', 'production'), 'PATH' => node[:rvm][:path]
 
   before_migrate do
     directory File.join(release_path, 'vendor')
@@ -29,6 +28,10 @@ deploy node[:denebola][:deploy_dir] do
 
     link File.join(release_path, 'vendor/bundle') do
       to File.join(node[:denebola][:deploy_dir], 'shared/bundle')
+    end
+
+    node[:denebola][:requirements].each do |package_name|
+      package package_name
     end
 
     execute 'rvm 2.2.0 do bundle install --path=vendor/bundle' do
