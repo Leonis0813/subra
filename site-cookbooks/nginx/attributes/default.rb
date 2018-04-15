080dcca179f605721596d6b565b33ef24b90200f
Leonis0813 <Leonis.0813@gmail.com>
2018-04-15 06:30:27 +0100

    install luajit, nginx_devel_kit and lua-nginx-module

--- site-cookbooks/nginx/attributes/default.rb
+++ site-cookbooks/nginx/attributes/default.rb
@@ -9,3 +9,22 @@ default[:nginx][:download] = {
 default[:nginx][:install_dir] = '/opt/nginx'
 default[:nginx][:apps] = []
 default[:nginx][:conf_files] = []
+default[:nginx][:luajit][:version] = '2.0.5'
+default[:nginx][:luajit][:src] = "LuaJIT-#{default[:nginx][:luajit][:version]}.tar.gz"
+default[:nginx][:luajit][:download] = {
+  :url => "http://luajit.org/download/#{default[:nginx][:luajit][:src]}",
+  :dir => '/tmp',
+}
+default[:nginx][:luajit][:install_dir] = '/usr/local/luajit'
+default[:nginx][:nginx_devel_kit][:version] = '0.3.0'
+default[:nginx][:nginx_devel_kit][:src] = "v#{default[:nginx][:nginx_devel_kit][:version]}.tar.gz"
+default[:nginx][:nginx_devel_kit][:download] = {
+  :url => "https://github.com/simplresty/ngx_devel_kit/archive/#{default[:nginx][:nginx_devel_kit][:src]}",
+  :dir => '/usr/local/src',
+}
+default[:nginx][:lua_nginx_module][:version] = '0.10.11'
+default[:nginx][:lua_nginx_module][:src] = "v#{default[:nginx][:lua_nginx_module][:version]}.tar.gz"
+default[:nginx][:lua_nginx_module][:download] = {
+  :url => "https://github.com/openresty/lua-nginx-module/archive/#{default[:nginx][:lua_nginx_module][:src]}",
+  :dir => '/usr/local/src',
+}
--- site-cookbooks/nginx/recipes/default.rb
+++ site-cookbooks/nginx/recipes/default.rb
@@ -28,8 +28,70 @@ unless File.exists?(node[:nginx][:install_dir])
     not_if { File.exists?(extracted_dir) }
   end
 
+  download_luajit_path = File.join(node[:nginx][:luajit][:download][:dir], node[:nginx][:luajit][:src])
+  extracted_luajit_dir = download_luajit_path.sub('.tar.gz', '')
+  remote_file download_luajit_path do
+    source node[:nginx][:luajit][:download][:url]
+    not_if { File.exists?(download_luajit_path) }
+  end
+
+  execute "tar zxf #{node[:nginx][:luajit][:src]}" do
+    cwd node[:nginx][:luajit][:download][:dir]
+    not_if { File.exists?(extracted_luajit_dir) }
+  end
+
   [
-    {:name => 'nginx configuration', :command => "./configure --prefix=#{node[:nginx][:install_dir]}"},
+    {:name => 'make LuaJIT', :command => "make PREFIX=#{node[:nginx][:luajit][:install_dir]}"},
+    {:name => 'install LuaJIT', :command => "sudo make install PREFIX=#{node[:nginx][:luajit][:install_dir]}"},
+  ].each do |resource|
+    execute resource[:name] do
+      command resource[:command]
+      cwd extracted_luajit_dir
+      only_if { File.exists?(extracted_luajit_dir) }
+    end
+  end
+
+  link '/usr/local/bin/luajit' do
+    to '/usr/local/luajit/bin/luajit'
+    link_type :symbolic
+  end
+
+  download_nginx_devel_kit_path = File.join(node[:nginx][:nginx_devel_kit][:download][:dir], node[:nginx][:nginx_devel_kit][:src])
+  remote_file download_nginx_devel_kit_path do
+    source node[:nginx][:nginx_devel_kit][:download][:url]
+    not_if { File.exists?(download_nginx_devel_kit_path) }
+  end
+
+  execute "tar zxf #{node[:nginx][:nginx_devel_kit][:src]}" do
+    cwd node[:nginx][:nginx_devel_kit][:download][:dir]
+    not_if { File.exists?(download_nginx_devel_kit_path.sub('.tar.gz', '')) }
+  end
+
+  download_lua_nginx_module_path = File.join(node[:nginx][:lua_nginx_module][:download][:dir], node[:nginx][:lua_nginx_module][:src])
+  remote_file download_lua_nginx_module_path do
+    source node[:nginx][:lua_nginx_module][:download][:url]
+    not_if { File.exists?(download_lua_nginx_module_path) }
+  end
+
+  execute "tar zxf #{node[:nginx][:lua_nginx_module][:src]}" do
+    cwd node[:nginx][:lua_nginx_module][:download][:dir]
+    not_if { File.exists?(download_lua_nginx_module_path.sub('.tar.gz', '')) }
+  end
+
+  options = [
+    ['prefix', node[:nginx][:install_dir]],
+    ['add-module', "#{node[:nginx][:nginx_devel_kit][:download][:dir]}/ngx_devel_kit-#{node[:nginx][:nginx_devel_kit][:version]}"],
+    ['add-module', "#{node[:nginx][:lua_nginx_module][:download][:dir]}/lua-nginx-module-#{node[:nginx][:lua_nginx_module][:version]}"],
+    ['with-ld-opt', '"-Wl,-rpath,$LUAJIT_LIB"'],
+  ].map {|key, value| "--#{key}=#{value}" }
+
+  execute 'nginx configuration' do
+    command "./configure #{options.join(' ')}"
+    cwd extracted_dir
+    environment 'LUAJIT_LIB' => '/usr/local/luajit/lib', 'LUAJIT_INC' => '/usr/local/luajit/include/luajit-2.0'
+  end
+
+  [
     {:name => 'make nginx', :command => 'make'},
     {:name => 'install nginx', :command => 'sudo make install'},
   ].each do |resource|
@@ -40,8 +102,6 @@ unless File.exists?(node[:nginx][:install_dir])
     end
   end
 
-  include_recipe 'nginx::conf'
-
   file '/etc/init.d/nginx' do
     content IO.read(File.absolute_path(File.dirname(__FILE__) + '/../files/default/nginx'))
     owner 'root'
@@ -49,6 +109,8 @@ unless File.exists?(node[:nginx][:install_dir])
     mode 0755
     action :create
   end
+
+  include_recipe 'nginx::conf'
 
   service 'nginx' do
     action [:start, :enable]
