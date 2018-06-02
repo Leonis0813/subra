321d627052e58797857740fcd77a8a9ab42aeed5
Leonis0813 <Leonis.0813@gmail.com>
2018-06-02 11:43:15 +0100

    create default recipe

--- site-cookbooks/rstudio/recipes/default.rb
+++ site-cookbooks/rstudio/recipes/default.rb
@@ -6,3 +6,21 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+package %w[epel-release R]
+
+remote_file '/tmp/rstudio-server-rhel-1.1.453-x86_64.rpm' do
+  source 'https://download2.rstudio.org/rstudio-server-rhel-1.1.453-x86_64.rpm'
+  owner 'root'
+  group 'root'
+  mode 0755
+  not_if { File.exists?('/tmp/rstudio-server-rhel-1.1.453-x86_64.rpm') }
+end
+
+package 'rstudio' do
+  source '/tmp/rstudio-server-rhel-1.1.453-x86_64.rpm'
+  not_if 'rpm -q rstudio'
+end
+
+execute 'rstudio-server start'
+
+execute 'chkconfig rstudio-server on'
