1ffb786076245811e1c5b139cb55249fc1d797de
Leonis <Leonis.0813@gmail.com>
2018-06-25 20:50:52 +0900

    Alterf update template (#129)
    * add feature attributes
    
    * update docs

--- site-cookbooks/alterf/attributes/default.rb
+++ site-cookbooks/alterf/attributes/default.rb
@@ -7,3 +7,15 @@ default[:alterf][:shared_dirs] = %w[ log bundle tmp/files ]
 default[:alterf][:requirements] = %w[ mysql-devel ]
 default[:alterf][:ruby_version] = '2.2.0'
 default[:alterf][:mysql_users] = %w[ development test ]
+default[:alterf][:features] = %w[
+  age
+  burden_weight
+  direction
+  distance
+  number
+  place
+  round
+  track
+  weather
+  weight
+]
