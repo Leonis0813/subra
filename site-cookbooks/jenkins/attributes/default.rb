016717ac224a31b203643135fa40f82adbda2b74
Leonis0813 <leonis.0813@gmail.com>
2018-07-07 03:33:29 +0000

    jenkins version up

--- site-cookbooks/jenkins/attributes/default.rb
+++ site-cookbooks/jenkins/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:jenkins][:version] = '2.40'
+default[:jenkins][:version] = '2.130'
 default[:jenkins][:rpm_file] = "jenkins-#{default[:jenkins][:version]}-1.1.noarch.rpm"
 default[:jenkins][:rpm_path] = "/tmp/#{default[:jenkins][:rpm_file]}"
 default[:jenkins][:rpm_url] = "http://pkg.jenkins-ci.org/redhat/#{default[:jenkins][:rpm_file]}"
