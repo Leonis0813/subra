6d1c01766c296661e1c93b1de51282a473031673
Leonis0813 <Leonis.0813@gmail.com>
2018-07-24 12:11:07 +0100

    add resource to install python

--- site-cookbooks/sphinx/recipes/install.rb
+++ site-cookbooks/sphinx/recipes/install.rb
@@ -6,6 +6,26 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+pyenv = node[:sphinx][:pyenv]
+python = node[:sphinx][:python]
+
+git pyenv[:root] do
+  repository pyenv[:repository]
+  revision pyenv[:revision]
+end
+
+execute "install python #{python[:version]}" do
+  command <<"EOF"
+pyenv init -
+pyenv install #{python[:version]}
+pyenv global #{python[:version]}
+pyenv rehash
+EOF
+  environment 'PYENV_ROOT' => pyenv[:root],
+              'PATH' => '$PYENV_ROOT/bin:$PATH'
+end
+
 package node[:sphinx][:requirements] do
   action :install
 end
