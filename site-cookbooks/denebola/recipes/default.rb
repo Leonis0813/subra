8a98a713c1ad1a0edca2253d292b7bbb0a54217f
Leonis0813 <Leonis.0813@gmail.com>
2018-08-12 03:47:02 +0100

    refactor denebola

--- site-cookbooks/denebola/recipes/default.rb
+++ site-cookbooks/denebola/recipes/default.rb
@@ -6,44 +6,13 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-deploy node[:denebola][:deploy_dir] do
-  repo node[:denebola][:repository]
-  branch ENV['DENEBOLA_VERSION'] || node[:denebola][:branch]
-  migrate true
-  create_dirs_before_symlink.clear
-  purge_before_symlink.clear
-  symlink_before_migrate.clear
-  symlinks node[:denebola][:symlinks]
-  migration_command "rvm #{node[:denebola][:ruby_version]} do bundle exec rake db:migrate"
-  environment 'RAILS_ENV' => node.chef_environment.sub('compute', 'production'), 'PATH' => node[:rvm][:path]
+ruby_version = node[:denebola][:ruby_version]
 
-  before_migrate do
-    directory File.join(release_path, 'vendor')
+rvm_ruby ruby_version
 
-    node[:denebola][:shared_dirs].each do |dir|
-      directory File.join(node[:denebola][:deploy_dir], "shared/#{dir}") do
-        recursive true
-      end
-    end
-
-    {'log' => 'log', 'vendor/bundle' => 'bundle'}.each do |from, to|
-      link File.join(release_path, from) do
-        to File.join(node[:denebola][:deploy_dir], "shared/#{to}")
-      end
-    end
-
-    node[:denebola][:requirements].each do |package_name|
-      package package_name
-    end
-
-    execute 'rvm 2.2.0 do bundle install --path=vendor/bundle' do
-      cwd release_path
-      environment 'PATH' => node[:rvm][:path]
-    end
-
-    execute 'rvm 2.2.0 do bundle exec rake db:reset' do
-      cwd release_path
-      environment 'RAILS_ENV' => node.chef_environment, 'PATH' => node[:rvm][:path]
-    end
-  end
+rvm_gem 'bundler' do
+  ruby_version ruby_version
+  gemset 'global'
 end
+
+include_recipe 'denebola::app'
