91f97a7e153575103518f30a451cf978040eaa16
Leonis0813 <Leonis.0813@gmail.com>
2018-11-24 05:30:14 +0000

    add shared dir to tmp

--- site-cookbooks/regulus/attributes/default.rb
+++ site-cookbooks/regulus/attributes/default.rb
@@ -3,7 +3,7 @@ default[:regulus][:deploy_dir] = "/opt/nginx/apps/#{default[:regulus][:app_name]
 default[:regulus][:repository] = "https://github.com/Leonis0813/#{default[:regulus][:app_name]}.git"
 default[:regulus][:branch] = 'develop'
 default[:regulus][:symlinks] = {'tmp' => 'tmp', 'log' => 'log', 'results' => 'results'}
-default[:regulus][:shared_dirs] = %w[ tmp log bundle results ]
+default[:regulus][:shared_dirs] = %w[ tmp log bundle results tmp/models ]
 default[:regulus][:requirements] = %w[ mysql-devel ]
 default[:regulus][:ruby_version] = '2.2.0'
 default[:regulus][:mysql_users] = %w[ development test ]
