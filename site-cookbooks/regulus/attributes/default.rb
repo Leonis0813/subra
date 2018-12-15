5176d0ccc984ee24ebf9a41cdfa3aea785e4f709
Leonis <Leonis.0813@gmail.com>
2018-12-15 23:27:48 +0900

    Regulus create shared dir (#157)
    * remove results and create tmp/files
    
    * update CHANGELOG.md

--- site-cookbooks/regulus/attributes/default.rb
+++ site-cookbooks/regulus/attributes/default.rb
@@ -2,8 +2,8 @@ default[:regulus][:app_name] = 'regulus'
 default[:regulus][:deploy_dir] = "/opt/nginx/apps/#{default[:regulus][:app_name]}"
 default[:regulus][:repository] = "https://github.com/Leonis0813/#{default[:regulus][:app_name]}.git"
 default[:regulus][:branch] = 'develop'
-default[:regulus][:symlinks] = {'tmp' => 'tmp', 'log' => 'log', 'results' => 'results'}
-default[:regulus][:shared_dirs] = %w[ tmp log bundle results tmp/models ]
+default[:regulus][:symlinks] = {'tmp' => 'tmp', 'log' => 'log'}
+default[:regulus][:shared_dirs] = %w[ tmp log bundle tmp/files tmp/models ]
 default[:regulus][:requirements] = %w[ mysql-devel ]
 default[:regulus][:ruby_version] = '2.2.0'
 default[:regulus][:mysql_users] = %w[ development test ]
