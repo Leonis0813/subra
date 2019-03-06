e33b62b084ea77ee13f7682411a3f9b0f23630a3
Leonis0813 <Leonis.0813@gmail.com>
2019-03-06 11:57:14 +0000

    overwrite if exists branch

--- site-cookbooks/jenkins/files/default/update_changelog.rb
+++ site-cookbooks/jenkins/files/default/update_changelog.rb
@@ -5,10 +5,16 @@ lines = File.read(CHANGELOG).lines
 versions = lines.first.match(/\A#\s(\d+)\.(\d+)\.(\d+)/)[1..3]
 
 File.open(CHANGELOG, 'w') do |file|
-  file.puts <<"EOF"
-# #{versions[0]}.#{versions[1]}.#{versions[2].to_i + 1} (#{Date.today.strftime('%Y/%m/%d')})
+  today = Date.today.strftime('%Y/%m/%d')
+
+  if system('git diff develop --name-only | grep "CHANGELOG"')
+    lines[0].sub!(/\d{4}\/\d{2}\/\d{2}/, today)
+  else
+    file.puts <<"EOF"
+# #{versions[0]}.#{versions[1]}.#{versions[2].to_i + 1} (#{today})
 - [UPDATE] gems
 
 EOF
+  end
   lines.each {|line| file.puts(line) }
 end
