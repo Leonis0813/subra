bb48bb36835b27ef374a3776bc592158ea0b8887
Leonis0813 <Leonis.0813@gmail.com>
2019-05-02 05:41:59 +0000

    update attribute

--- site-cookbooks/denebola/attributes/default.rb
+++ site-cookbooks/denebola/attributes/default.rb
@@ -5,7 +5,7 @@ default[:denebola][:branch] = 'develop'
 default[:denebola][:symlinks] = {'backup' => 'backup'}
 default[:denebola][:shared_dirs] = %w[ backup/race_list backup/races log bundle ]
 default[:denebola][:requirements] = %w[ mysql-devel ]
-default[:denebola][:ruby_version] = '2.3.7'
+default[:denebola][:ruby_version] = '2.4.4'
 default[:denebola][:export_dir] = '/etc/exports.d'
 default[:denebola][:exports] = [
   {:path => "#{default[:denebola][:deploy_dir]}/backup", :ips => '*', :options => %w[ rw no_root_squash ]}
