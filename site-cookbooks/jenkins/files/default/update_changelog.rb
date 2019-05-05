77b4ab7bf3af5d5e3969e12195e5d6f35f8c23bf
Leonis0813 <Leonis.0813@gmail.com>
2019-05-05 10:53:52 +0000

    fix Style/RegexpLiteral

--- site-cookbooks/jenkins/files/default/update_changelog.rb
+++ site-cookbooks/jenkins/files/default/update_changelog.rb
@@ -9,7 +9,7 @@ File.open(CHANGELOG, 'w') do |file|
   today = Date.today.strftime('%Y/%m/%d')
 
   if changed
-    lines[0].sub!(/\d{4}\/\d{2}\/\d{2}/, today)
+    lines[0].sub!(%r{\d{4}/\d{2}/\d{2}}, today)
   else
     file.puts <<"EOF"
 # #{versions[0]}.#{versions[1]}.#{versions[2].to_i + 1} (#{today})
