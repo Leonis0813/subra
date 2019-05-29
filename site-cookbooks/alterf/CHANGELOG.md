2e1fdfefa5684055a4320b35c0c56fda06b204a5
Leonis <Leonis.0813@gmail.com>
2019-05-29 20:45:48 +0900

    Alterf update settings (#192)
    * update template
    
    * add new attributes for template
    
    * fix attribute
    
    * merge into config/settings.yml
    
    * add evaluation setting
    
    * entries to horses
    
    * update README.md
    
    * update CHANGELOG.md

--- site-cookbooks/alterf/CHANGELOG.md
+++ site-cookbooks/alterf/CHANGELOG.md
@@ -2,6 +2,9 @@
 
 This file is used to list changes made in each version of the alterf cookbook.
 
+## 2.1.0
+- Change template to config/settings.yml
+
 ## 2.0.6
 - Fix coding style
 
--- site-cookbooks/alterf/attributes/default.rb
+++ site-cookbooks/alterf/attributes/default.rb
@@ -8,18 +8,3 @@ default[:alterf][:shared_dirs] = %w[log bundle tmp/files]
 default[:alterf][:requirements] = %w[mysql-devel]
 default[:alterf][:ruby_version] = '2.4.4'
 default[:alterf][:mysql_users] = %w[development test]
-default[:alterf][:features] = %w[
-  age
-  burden_weight
-  direction
-  distance
-  grade
-  number
-  place
-  race_id
-  round
-  track
-  weather
-  weight
-  weight_diff
-]
