6cef8cc089347e684ab8c0fa7abe47c9cc5cf5d9
Leonis0813 <Leonis.0813@gmail.com>
2019-07-07 02:22:50 +0000

    update cookies table schema

--- site-cookbooks/mysql/files/default/create_table_cookies.sql
+++ site-cookbooks/mysql/files/default/create_table_cookies.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS cookies (
   id int(11) AUTO_INCREMENT PRIMARY KEY,
   value varchar(255) NOT NULL UNIQUE,
   user_id int(11) NOT NULL,
-  created_at datetime NOT NULL,
-  updated_at datetime NOT NULL
+  created_at datetime DEFAULT CURRENT_TIMESTAMP,
+  updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )
