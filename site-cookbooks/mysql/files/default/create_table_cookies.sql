66b39da3e52ebe395e3e6b1fd79457e898581799
Leonis0813 <Leonis.0813@gmail.com>
2019-07-07 02:35:08 +0000

    modify unique

--- site-cookbooks/mysql/files/default/create_table_cookies.sql
+++ site-cookbooks/mysql/files/default/create_table_cookies.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS cookies (
   value varchar(255) NOT NULL UNIQUE,
   user_id int(11) NOT NULL,
   created_at datetime DEFAULT CURRENT_TIMESTAMP,
-  updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  UNIQUE (value, user_id)
 )
