5d77dfb768127497b1d5236a54e8a1728a383c23
Leonis0813 <Leonis.0813@gmail.com>
2019-08-02 13:38:11 +0000

    update regulus ruby version

--- site-cookbooks/regulus/attributes/default.rb
+++ site-cookbooks/regulus/attributes/default.rb
@@ -6,6 +6,6 @@ default[:regulus][:branch] = 'develop'
 default[:regulus][:symlinks] = {'tmp' => 'tmp', 'log' => 'log'}
 default[:regulus][:shared_dirs] = %w[tmp log bundle tmp/files tmp/models]
 default[:regulus][:requirements] = %w[cmake mysql-devel]
-default[:regulus][:ruby_version] = '2.4.4'
+default[:regulus][:ruby_version] = '2.5.5'
 default[:regulus][:mysql_users] = %w[development test]
 default[:regulus][:python_packages] = %w[mysql-connector-python-rf pyyaml]
