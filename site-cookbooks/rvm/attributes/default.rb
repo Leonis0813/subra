2fba86045d271d27f29d04e06da784d3f1b04517
Leonis0813 <Leonis.0813@gmail.com>
2019-09-01 03:53:24 +0000

    [rvm] fix attribute

--- site-cookbooks/rvm/attributes/default.rb
+++ site-cookbooks/rvm/attributes/default.rb
@@ -1,12 +1,7 @@
 default[:rvm][:requirements] = %w[sudo which]
-default[:gpg][:server] = 'https://rvm.io/mpapis.asc'
-default[:rvm] = {
-  server: 'https://get.rvm.io',
-  install_dir: '/usr/local/rvm',
-  path: '/usr/local/rvm/bin:/usr/bin:/bin',
-}
+default[:rvm][:install_dir] = '/usr/local/rvm'
 default[:rvm][:install_commands] = [
-  "curl -sSL #{default[:gpg][:server]} | gpg --import -",
-  "curl -sSL #{default[:rvm][:server]} | sudo bash -s stable",
+  'curl -sSL https://rvm.io/mpapis.asc | gpg --import -',
+  'curl -sSL https://get.rvm.io | sudo bash -s stable',
   "#{default[:rvm][:install_dir]}/bin/rvm reload",
 ]
