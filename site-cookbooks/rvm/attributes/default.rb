d45c749e20e0fed1e21873325f4358dfc14f12fb
Leonis0813 <Leonis.0813@gmail.com>
2019-09-01 03:44:23 +0000

    [rvm] add required package

--- site-cookbooks/rvm/attributes/default.rb
+++ site-cookbooks/rvm/attributes/default.rb
@@ -1,3 +1,4 @@
+default[:rvm][:requirements] = %w[sudo which]
 default[:gpg][:server] = 'https://rvm.io/mpapis.asc'
 default[:rvm] = {
   server: 'https://get.rvm.io',
--- site-cookbooks/rvm/recipes/default.rb
+++ site-cookbooks/rvm/recipes/default.rb
@@ -6,7 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-package 'sudo'
+package node[:rvm][:requirements]
 
 node[:rvm][:install_commands].each do |command|
   execute command do
