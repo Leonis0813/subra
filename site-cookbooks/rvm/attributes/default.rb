9ab4d3f8c310f781d8c6de535b69f189bfb6debd
Leonis0813 <Leonis.0813@gmail.com>
2019-09-01 03:56:55 +0000

    [rvm] update install command

--- site-cookbooks/rvm/attributes/default.rb
+++ site-cookbooks/rvm/attributes/default.rb
@@ -1,7 +1,8 @@
 default[:rvm][:requirements] = %w[sudo which]
 default[:rvm][:install_dir] = '/usr/local/rvm'
 default[:rvm][:install_commands] = [
-  'curl -sSL https://rvm.io/mpapis.asc | gpg --import -',
+  'curl -sSL https://rvm.io/mpapis.asc | sudo gpg2 --import -',
+  'curl -sSL https://rvm.io/pkuczynski.asc | sudo gpg2 --import -',
   'curl -sSL https://get.rvm.io | sudo bash -s stable',
   "#{default[:rvm][:install_dir]}/bin/rvm reload",
 ]
