7d647628c24feafedf80cb3421476d9bf02dd2a8
Leonis0813 <Leonis.0813@gmail.com>
2019-10-09 12:00:03 +0000

    mkdir tmp/models/tensorboard

--- site-cookbooks/regulus/attributes/default.rb
+++ site-cookbooks/regulus/attributes/default.rb
@@ -4,7 +4,14 @@ default[:regulus][:repository] =
   "https://github.com/Leonis0813/#{default[:regulus][:app_name]}.git"
 default[:regulus][:branch] = 'develop'
 default[:regulus][:symlinks] = {'tmp' => 'tmp', 'log' => 'log'}
-default[:regulus][:shared_dirs] = %w[tmp log bundle tmp/files tmp/models]
+default[:regulus][:shared_dirs] = %w[
+  tmp
+  log
+  bundle
+  tmp/files
+  tmp/models
+  tmp/models/tensorboard
+]
 default[:regulus][:requirements] = %w[cmake mysql-devel cronie-noanacron]
 default[:regulus][:ruby_version] = '2.5.5'
 default[:regulus][:mysql_users] = %w[development test]
